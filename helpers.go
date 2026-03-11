35085a7d192696db412dd2ea463bc74eb6c89bb1
imubuda <262111828+imubuda@users.noreply.github.com>
2026-03-11 17:41:53 +1000

    Handle unexpected API errors gracefully

--- helpers.go
+++ helpers.go
@@ -6,3 +6,10 @@ func min(a, b int) int {
     return b
 }
 
+
+type WebResponse struct {
+    Success bool        `json:"success"`
+    Data    interface{} `json:"data,omitempty"`
+    Error   string      `json:"error,omitempty"`
+}
+
